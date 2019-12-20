4bf99ad4ac7ff62a4e9621c1089d7679b6270a5f
Felix Hamann <felix.fischeversenker@gmail.com>
2019-12-20 23:40:28 +0100

    chore: cleanup

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const rootStyle = document.documentElement.style;
 const computedRootStyle = getComputedStyle(document.documentElement);
 const dotRadius = parseInt(computedRootStyle.getPropertyValue('--dot-radius'));
-const scale = coord => coord - dotRadius / 2;
-const mouseMoveListener = evt => window.requestAnimationFrame(() => {
-  rootStyle.setProperty('--mouse-x', scale(evt.clientX));
-  rootStyle.setProperty('--mouse-y', scale(evt.clientY));
+const wrapperElement = document.querySelector('.wrapper');
+const wrapperRect = wrapperElement.getBoundingClientRect();
+
+const scaleMouseCoordinate = coord => coord - dotRadius / 2;
+const mouseMoveListener = (e) => window.requestAnimationFrame(() => {
+  rootStyle.setProperty('--mouse-x', scaleMouseCoordinate(e.clientX - wrapperRect.left));
+  rootStyle.setProperty('--mouse-y', scaleMouseCoordinate(e.clientY - wrapperRect.top));
 });
 
-const gridChilds = Array.from(document.querySelectorAll('.grid-child'));
-const markAllUnselected = () => gridChilds.forEach(gridChild => gridChild.classList.remove('grid-child--active'));
+const gridChildren = Array.from(document.querySelectorAll('.grid-child'));
+const markAllUnselected = () => gridChildren.forEach(gridChild => gridChild.classList.remove('grid-child--active'));
 const markSelected = (gridChild) => gridChild.classList.add('grid-child--active');
-const gridChildClicked = (evt) => {
-  if (!evt.ctrlKey) {
+const gridChildClicked = (e) => {
+  if (!e.ctrlKey) {
     markAllUnselected();
   }
-  markSelected(evt.target);
+
+  markSelected(e.target);
+
   if (window.getSelection) {
     window.getSelection().removeAllRanges();
   } else if (document.selection) {
@@ -22,6 +27,5 @@ const gridChildClicked = (evt) => {
   }
 };
 
-document.documentElement.addEventListener('mousemove', mouseMoveListener);
-
-gridChilds.forEach(gc => gc.addEventListener('click', gridChildClicked))
+wrapperElement.addEventListener('mousemove', mouseMoveListener);
+gridChildren.forEach(gridChild => gridChild.addEventListener('click', gridChildClicked))
